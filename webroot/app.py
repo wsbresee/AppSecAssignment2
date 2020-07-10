8b47c4b56a515bcda2d462a46dcbab7671fb851a
Willy Bresee <wsbresee@gmail.com>
2020-07-10 12:29:35 -0400

    got sessions working, patched some additional vulnerabilities

--- webroot/app.py
+++ webroot/app.py
@@ -23,6 +23,8 @@ def get_user(the_user):
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
+app.config.update(SESSION_COOKIE_SECURE=True, SESSION_COOKIE_HTTPONLY=True, SESSION_COOKIE_SAMESITE='Lax')
+app.config.update(PERMANENT_SESSION_LIFETIME=600)
 app.secret_key = SECRET_KEY
 csrf = CSRFProtect(app)
 
@@ -31,15 +33,28 @@ def spell_check():
     textout = None
     misspelled = None
     if (request.method == 'POST'):
-        inputtext = request.form['inputtext']
-        inputtext_file = open(INPUTTEXT_ADDR, 'w')
-        inputtext_file.write(inputtext)
-        inputtext_file.close()
-        textout=inputtext
-        misspelled = check_output(['./a.out', INPUTTEXT_ADDR, DICTIONARY_ADDR]).decode('utf-8')
-        misspelled = misspelled.replace('\n', ',').strip(',')
+        if 'username' in session:
+            username = session['username']
+            password = session['password']
+            phone = session['phone']
+            user = get_user(User(username, password, phone))
+        else:
+            user = None
+        if user and user.password == password and user.phone == phone and user.is_logged_in:
+            inputtext = request.form['inputtext']
+            inputtext_file = open(INPUTTEXT_ADDR, 'w')
+            inputtext_file.write(inputtext)
+            inputtext_file.close()
+            textout=inputtext
+            misspelled = check_output(['./a.out', INPUTTEXT_ADDR, DICTIONARY_ADDR]).decode('utf-8')
+            misspelled = misspelled.replace('\n', ',').strip(',')
+        else:
+            textout = "Invalid user. Please log in."
     response = make_response(render_template('spell_check.html', textout=textout, misspelled=misspelled))
     response.headers['Content-Security-Policy'] = "default-src 'self'"
+    response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
+    response.headers['X-Frame-Options'] = 'SAMEORIGIN'
+    response.headers['X-XSS-Protection'] = '1; mode=block'
     return response
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -66,9 +81,15 @@ def login():
                 error = 'Two-factor failure'
             else:
                 error = 'success'
+                session['username'] = username
+                session['password'] = password
+                session['phone'] = phone
                 maybe_the_user.is_logged_in = True
     response = make_response(render_template('login.html', error=error))
     response.headers['Content-Security-Policy'] = "default-src 'self'"
+    response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
+    response.headers['X-Frame-Options'] = 'SAMEORIGIN'
+    response.headers['X-XSS-Protection'] = '1; mode=block'
     return response
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -93,6 +114,9 @@ def register():
                 error = 'failure'
     response = make_response(render_template('register.html', error=error))
     response.headers['Content-Security-Policy'] = "default-src 'self'"
+    response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
+    response.headers['X-Frame-Options'] = 'SAMEORIGIN'
+    response.headers['X-XSS-Protection'] = '1; mode=block'
     return response
 
 if (__name__ == '__main__'):
